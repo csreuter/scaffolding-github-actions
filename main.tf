d24d756ae05014500ec8e3f4918df08543c7a1cf
Chris Reuter <88755934+csreuter@users.noreply.github.com>
2024-10-07 11:50:52 -0400

    [Resourcely Instance] test

--- main.tf
+++ main.tf
@@ -5,3 +5,31 @@ resource "null_resource" "foo" {
     foo = var.project
   }
 }
+
+resource "aws_s3_bucket" "test_SJgAzFfpM4xyaXAi" {
+  bucket = "test"
+}
+
+resource "aws_s3_bucket_public_access_block" "test_SJgAzFfpM4xyaXAi" {
+  bucket                  = aws_s3_bucket.test_SJgAzFfpM4xyaXAi.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "test_SJgAzFfpM4xyaXAi" {
+  bucket = aws_s3_bucket.test_SJgAzFfpM4xyaXAi.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "test_SJgAzFfpM4xyaXAi" {
+  bucket = aws_s3_bucket.test_SJgAzFfpM4xyaXAi.id
+
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
